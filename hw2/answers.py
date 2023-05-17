f114dd9e54e8ccb7b1649753c4ba483d37da04df
GuyPgit <mibindabar@gmail.com>
2023-05-17 18:24:07 +0300

    - completed answers for part 2

--- hw2/answers.py
+++ hw2/answers.py
@@ -55,28 +55,21 @@ def part2_dropout_hp():
 
 part2_q1 = r"""
 **Your answer:**
+1. Overall, the graphs showing the results match what we expected to see.
+For dropout = 0 (no dropout), we get overfitting, as when the training loss keeps decreasing, the test loss starts to go up at some point, and the test accuracy cease to improve.
+This is to be expected, as the dataset is small and we don't get good generalization. 
+Using dropout allowed us to achieve better generalization, as we can see that the test loss keeps decreasing and the accuracy increases as we progress through the epochs. 
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+2. Using dropout = 0.4 yielded better test results (lower test loss and higher test accuracy) compared to dropout = 0.8. 
+With dropout = 0.8 we get bad results due to dropping too many nodes, which significantly hinders our abillity to fit well.
 """
 
 part2_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+Indeed, it is possible for the test loss to increase for a few epochs while the test accuracy also increases.
+The loss is measured through as continuous loss function, while the accuracy is computed by the percentage of how many labels were predicted correctly.
+We can encounter a situation where a few steps were taken in the direction that caused the total test loss to increase, yet at the same time it just so happens that the number of labels which were predicted correctly increased abit.
+In general, with the steps we hope to eventually converge into the lowest minimum of the loss function, but along the way we can encounter positive slopes that we need to go through in order to eventually reach it.
 """
 # ==============
 
